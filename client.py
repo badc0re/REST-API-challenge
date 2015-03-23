85a6288932179371ac6a0203f9ac870ef3ba1087
badc0re <dame.jovanoski@gmail.com>
2015-03-23 22:26:13 +0100

    hard stone is setted up

--- client.py
+++ client.py
@@ -1,1 +1,9 @@
-#test
+import requests
+
+
+class Client(object):
+    def __init__(self):
+        self.api_url = 'http://localhost:8080/'
+
+    def get_json_data(self):
+        pass
--- server.py
+++ server.py
@@ -1,1 +1,26 @@
-#test
+import web
+
+
+urls = (
+    '/', 'index'
+)
+
+
+class index:
+    def POST(self):
+        data = None
+        try:
+            i = web.input()
+            print i
+            data = web.data()
+            print data
+            return
+        except Exception, e:
+            print e
+        print data
+
+
+if __name__ == "__main__":
+    app = web.application(urls,
+                          globals())
+    app.run()
