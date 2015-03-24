4e4506f453146142f8170f88a1e157e2738522b1
Dame <dame.jovanoski@gmail.com>
2015-03-24 14:58:19 +0100

    Added doc strings, removed some vars.

--- error_request.py
+++ error_request.py
@@ -2,10 +2,13 @@ import web
 
 
 class NotAcceptable(web.HTTPError):
-    ''' Error Status 406 '''
+    ''' 
+        Extend HTTP status to 406.
+        :returns: HTTP error with error status 406
+    '''
 
     def __init__(self, message=None):
-        self.message = "not acceptable"
+        message = "Not acceptable!"
         status = "406 Not Acceptable"
         headers = {'Content-Type': 'text/html'}
-        web.HTTPError.__init__(self, status, headers, message or self.message)
+        web.HTTPError.__init__(self, status, headers, message)
