ad8c82f33889a1246d0101cab2038e26fdfce695
Dame <dame.jovanoski@gmail.com>
2015-03-24 15:39:28 +0100

    Update client.py

--- client.py
+++ client.py
@@ -8,15 +8,19 @@ class Client(object):
         self.data_file = 'data/dataset.jl'
 
     def get_json_data(self):
-        '''
-            Loads test data from file.
+        '''Loads test data from file.
+        
+        :rtype dict: dictionary data
+        :returns: data from file
         '''
         with open(self.data_file) as json_file:
             return json.loads(json_file.read())
 
     def post_requests(self, json_post_data=None):
-        '''
-            Post requests to API, json POST requests.
+        '''Post requests to API, json POST requests.
+            
+        :type json_post_data: dict
+        :param json_post_data: movie data in json format
         '''
         headers = {'Content-type': 'application/json',
                    'Accept': 'text/plain'}
@@ -27,9 +31,8 @@ class Client(object):
         print post_request.text
 
     def make_request_to_api(self):
-        '''
-            Makes json post requests to server.
-            Json data is from file.
+        '''Makes json post requests to server.
+           Json data is from file.
         '''
         for json_post_data in self.get_json_data():
             self.post_requests(json_post_data=json_post_data)
